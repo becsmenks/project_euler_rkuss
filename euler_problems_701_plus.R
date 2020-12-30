7a95ff8dad85de84990970a65d023b26688aaff2
becsmenks <beccakuss@gmail.com>
2020-12-30 01:25:54 -0700

    starting secret santa problem

---
+++ euler_problems_701_plus.R
@@ -0,0 +1,60 @@
+# Project Euler
+# projecteuler.net
+# Becca Kuss
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+
+source('~/git/project_euler_rkuss/euler_functions.R')
+
+
+# Problem 740 - Secret Santa ----------------------------------------------
+
+# Secret Santa is a process that allows n people to give each other presents, so 
+# that each person gives a single present and receives a single present. At the 
+# beginning each of the n people write their name on a slip of paper and put the 
+# slip into a hat. Each person takes a random slip from the hat. If the slip has 
+# their name they draw another random slip from the hat and then put the slip 
+# with their name back into the hat. At the end everyone buys a Christmas 
+# present for the person whose name is on the slip they are holding. This 
+# process will fail if the last person draws their own name.
+# 
+# In this variation each of the n people gives and receives two presents. At the 
+# beginning each of the n people writes their name on two slips of paper and 
+# puts the slips into a hat (there will be 2n slips of paper in the hat). As 
+# before each person takes a random slip from the hat that does not contain 
+# their name. Then they do the same process again so that they end up with two 
+# slips neither of which contain their name. The process will fail if the last 
+# person gets at least one slip with their own name.
+# 
+# Define q(n) to be the probability of this happening. You are given 
+# q(3) = 0.3611111111 and q(5) = 0.2476095994 both rounded to 10 decimal places.
+# 
+# Find q(100) rounded to 10 decimal places.
+
+# Theory:
+# q(3) = p(p1 doesn't pick both p3) & 
+#           p(p2 doesn't pick both p3 | p1 doesn't pick both p3)
+
+# q(5) = p(p1 doesn't pick both p5) & 
+#           p(p2 doesn't pick both p5 | p1 doesn't pick both p5) & 
+#           p(p3 doesn't pick both p5 | p1&p2 don't pick both p5) & 
+#           p(p4 doesn't pick both p5 | p1&p2&p3 don't pick both p5)
+
+
+n <- 3
+
+# p(p1 doesn't pick both pn) = p(p1 pick other) & p(p1 pick other | pick other)
+first_prob_num <- 2*n - 2 - 2 # subtract 2 for p1, 2 for pn
+first_prob_den <- 2*n - 2 # subtract 2 for p1
+second_prob_num <- 2*n - 2 - 2 - 1 # subtract 2 for p1, 2 for pn, 1 for first draw
+second_prob_den <- 2*n - 2 - 1 # subtract 2 for p1, 1 for first draw
+
+first_prob <- (first_prob_num / first_prob_den)
+second_prob <- (second_prob_num / second_prob_den)
+
+p1_prob_success <- first_prob * second_prob
+# ^^ this is wrong because p1 doesn't have to pick both non-p3, there can be
+# at most one p3 slip
