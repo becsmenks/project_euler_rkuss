9915e7713283cffb445991d81eabdff7b3389099
becsmenks <beccakuss@gmail.com>
2021-01-02 20:29:13 -0700

    seems like the concave triangle one should be solvable...

--- euler_problems_501_600.R
+++ euler_problems_501_600.R
@@ -28,4 +28,42 @@ quad_combos <- data.frame(a = 1:m) %>%
   merge(data.frame(d = 1:m)) %>% 
   add_rownames("combo")
 
+# Problem 587- Concave triangle -------------------------------------------
+
+# A square is drawn around a circle as shown in the diagram below on the left.
+# We shall call the blue shaded region the L-section.
+# A line is drawn from the bottom left of the square to the top right as shown 
+# in the diagram on the right.
+# We shall call the orange shaded region a concave triangle.
+# 
+# It should be clear that the concave triangle occupies exactly half of the 
+# L-section.
+# 
+# Two circles are placed next to each other horizontally, a rectangle is drawn 
+# around both circles, and a line is drawn from the bottom left to the top right 
+# as shown in the diagram below.
+# 
+# This time the concave triangle occupies approximately 36.46% of the L-section.
+# 
+# If n circles are placed next to each other horizontally, a rectangle is drawn 
+# around the n circles, and a line is drawn from the bottom left to the top 
+# right, then it can be shown that the least value of n for which the concave 
+# triangle occupies less than 10% of the L-section is n = 15.
+# 
+# What is the least value of n for which the concave triangle occupies less than 
+# 0.1% of the L-section?
+
+r <- 5
+n_circles <- 2
+
+# Start by calculating the area of the L-section based on the circle's radius
+area_circle <- pi * r^2
+area_square <- (2 * r)^2
+area_l <- (area_square - area_circle) / 4
+
+# Calculate the x,y coordinates where the diagonal line intersects the circle
+y <- (((2*n_circles*r) + (2*r)) - sqrt((((2*n_circles*r) - (2*r))^2) - (4*((n^2) + 1)*(r^2)))) /
+  (2 * ((n^2) + 1))
+x <- y * n_circles
+
 
