0c7f78ee5360ec178372c669471fc380a7316ff8
becsmenks <beccakuss@gmail.com>
2021-01-21 20:35:54 -0700

    solved huge nonmersenne prime

--- euler_problems_python/euler_problems_001_100.py
+++ euler_problems_python/euler_problems_001_100.py
@@ -488,11 +488,31 @@ for n in range(1, 10000001):
     if n == 89:
         n_89 += 1
 
+# Problem 97 - Large non-Mersenne prime ------------------------------------
 
+# The first known prime found to exceed one million digits was discovered in
+# 1999, and is a Mersenne prime of the form 2^6972593−1; it contains exactly
+# 2,098,960 digits. Subsequently other Mersenne primes, of the form 2^p−1,
+# have been found which contain more digits.
+#
+# However, in 2004 there was found a massive non-Mersenne prime which contains
+# 2,357,207 digits: 28433×2^7830457+1.
+#
+# Find the last ten digits of this prime number.
 
+n = (28433 * (2 ** 7830457)) + 1
+i = 0
+last_ten = 0
+while i < 10:
+    # Pull out the right-most digit and add to total
+    last_ten = last_ten + ((n % 10) * (10 ** i))
 
+    # Update the remaining digits
+    n = n // 10
 
+    # Count the digit
+    i += 1
 
-
+last_ten
 
 
